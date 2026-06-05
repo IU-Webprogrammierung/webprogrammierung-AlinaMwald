376f3cac2434c6734dcc5dd7d3394c3bcbd79252
Alina Mirwald <68157658+AlinaMwald@users.noreply.github.com>
2026-06-05 23:07:52 +0200

    Add active state to nav links
    automatic active state with js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-// JavaScript for loading header and footer components
+/* JavaScript for loading header and footer components */
 loadComponent("header", "components/header.html");
 loadComponent("footer", "components/footer.html");
 
 async function loadComponent(selector, path) {
-  const response = await fetch(path); 
-  const html = await response.text(); // nur html Teil geben, also nur Text und dann in HTML speichern
+  const response = await fetch(path);
+  const html = await response.text();
+
   const element = document.querySelector(selector);
   element.innerHTML = html;
+
+  if (selector === "header") {
+    setActiveNavLink();
+  }
 }
 
-// JavaScript for responsive navigation menu
+/* JavaScript for setting active navigation link */
+function setActiveNavLink() {
+  const currentPath = window.location.pathname.split("/").pop() + window.location.hash;
+
+  document.querySelectorAll(".nav-link").forEach(link => {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("active");
+    }
+  });
+}
+
+/* JavaScript for responsive navigation menu */
 function myFunction() {
   const navbar = document.getElementById("topNavbar");
   if (!navbar) return;
